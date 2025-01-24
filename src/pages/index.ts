c5ca39d69657b4788c7fbd7939848d1759cee8e7
JOY <92291790+duwlsssss@users.noreply.github.com>
2025-01-24 19:00:22 +0900

    [🔧 Fix] 이벤트 상세 폴더 이름 변경 (#44)
    fix: 폴더명 변경 (#43)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,5 @@
 export { default as EventPage } from './event/index';
 export { default as EventMapPage } from './event/map';
 export { default as EventSearchPage } from './event/search';
-export { default as EventDetailPage } from './event/:id';
+export { default as EventDetailPage } from './event/[id]';
 export { default as EventScrapPage } from './event/scrap';
