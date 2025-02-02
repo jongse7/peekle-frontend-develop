c834e5f7f58e5a28c878df7a99743f96195e5acd
rail <78732904+jongse7@users.noreply.github.com>
2025-02-02 17:16:53 +0900

    fix: react-helmet-async 코드 복구 (#64)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -9,17 +10,17 @@ if (container.hasChildNodes()) {
   ReactDOM.hydrateRoot(
     container,
     <StrictMode>
-      {/* <HelmetProvider> */}
-      <App />
-      {/* </HelmetProvider> */}
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </StrictMode>,
   );
 } else {
   root.render(
     <StrictMode>
-      {/* <HelmetProvider> */}
-      <App />
-      {/* </HelmetProvider> */}
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </StrictMode>,
   );
 }
